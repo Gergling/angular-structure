8537982de8721d1cf10113822370f00303d58af0
Greg <gregmichaeldavies@gmail.com>
2015-10-12 09:55:14 +0100

    Create controller.js

---
+++ example/application/controller.js
@@ -0,0 +1,5 @@
+// There is one controller for this module, so we just name it 'applicationController'.
+
+angular.module('application').controller('applicationController', function () {
+    this.buttons = [];
+});
