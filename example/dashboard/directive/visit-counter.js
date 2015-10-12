0a21c884e5f45f62740df7b0fdd567deddf9978d
Greg <gregmichaeldavies@gmail.com>
2015-10-12 10:17:12 +0100

    Create visit-counter.js

---
+++ example/dashboard/directive/visit-counter.js
@@ -0,0 +1,20 @@
+// Directive will be implemented with <element dashboard-visit-counter></element>.
+// Directive name is prefixed with module name to ensure it is unique.
+// Unlike controllers, services, factories and providers, directives are not injected and do not require such differentiation.
+
+angular.module('dashboard').directive('dashboardVisitCounter', function () {
+    return {
+        restrict: 'A',
+        scope: {class: '=dashboardVisitCounter'},
+        bindToController: true,
+        templateUrl: 'dashboard/partial/visit-counter.html',
+
+        // Verbose as this is, the variable in the partial will be clear and unique.
+        controllerAs: 'dashboardControllerVisitCounter',
+
+        // Use of controllerAs means that $scope variables can be assigned to 'this'.
+        controller: function (dashboardServiceVisitCounter) {
+            this.visits = dashboardServiceVisitCounter.visits;
+        }
+    };
+});
